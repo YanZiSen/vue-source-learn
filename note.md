bcc7a63484839307e122dc5a6d8c29a8367c7371
严宁宁 <yanningning@jiuqi.com.cn>
2020-06-05 16:29:03 +0800

    init

---
+++ note.md
@@ -0,0 +1,7 @@
+## import Vue 入口 src/platforms/web/runtime/entry-runtime-with-compiler
+## new Vue()
+### initState
+### data 与 data key代理
+### mount 方法 src/platform/web/runtime/inex.js
+### render 函数
+### update src/core/instance/lifecycle patch src/runtime/index
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'Hello Word'
+    }
+  },
+  mounted () {
+    console.log(this.msg)
+  }
+}
+</script>
+
+<style>
+</style>
